76870e317a1a1d6bc083d62c4ff6b8c3711c06b0
Aagat <aagatpokhrel@gmail.com>
2023-05-13 00:02:24 +0545

    extract completed

--- pipeline/core/extract.py
+++ pipeline/core/extract.py
@@ -1,2 +1,59 @@
+#this site extracts data from the imdb site by crawling using beautiful soup
+
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+shows_url = "https://www.imdb.com/calendar/?ref_=rlm&region=US&type=TV"
+movies_url = "https://www.imdb.com/calendar/?region=us"
+
+date = datetime.datetime.now()
+
+def crawl_url(url):
+    url = "https://www.imdb.com" + url
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(response.content, 'html.parser')
+    description = 0
+    genres = []
+    try:
+        description = soup.find('span',{'data-testid': 'plot-xl'})
+        description = description.text
+    except:
+        pass
+
+    try:
+        genres = []
+        find_genres = soup.find_all('span',{'class': 'ipc-chip__text'})
+        for genre in find_genres:
+            genres.append(genre.text)
+    except:
+        pass
+    return genres[:-1], description
+    
+def parse_article(soup):
+    objects = {}
+    article = soup.find('article',{'data-testid': 'calendar-section'})
+    try:
+        a_tags = article.find_all('a',{'role': 'button'})
+        for a_tag in a_tags:
+            title = a_tag.text
+            url = a_tag.get('href')
+            genres, descriptions = crawl_url(url)
+            objects[title] = {'genres': genres, 'description': descriptions}
+    except AttributeError:
+        pass
+    return objects
+    
+
 def extract():
-    pass
+    response_movies = requests.get(movies_url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup_movies = BeautifulSoup(response_movies.content, 'html.parser')
+
+    response_shows = requests.get(shows_url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup_shows = BeautifulSoup(response_shows.content, 'html.parser')
+    
+    movie_dictionary = parse_article(soup_movies)
+    show_dictionary = parse_article(soup_shows)
+
+    df = [movie_dictionary, show_dictionary]
+    return df
--- pipeline/core/transform.py
+++ pipeline/core/transform.py
@@ -1,2 +1,18 @@
-def transform(df):
+
+def transform_description(description):
     pass
+
+def merge_transform(df):
+    movie_dictionary = df[0]
+    show_dictionary = df[1]
+    #specify type of media
+    movie_dictionary = {k: {'genres': v['genres'], 'description': v['description'], 'desc_embedding':transform_description(v['description']) ,'type': 'movie'} for k, v in movie_dictionary.items()}
+    show_dictionary = {k: {'genres': v['genres'], 'description': v['description'], 'description': v['description'], 'desc_embedding':transform_description(v['description']) ,'type': 'show'} for k, v in show_dictionary.items()}
+    #merge the two dictionaries
+    movie_dictionary.update(show_dictionary)
+    return movie_dictionary
+
+
+def transform(df):
+    df = merge_transform(df)
+    return df
