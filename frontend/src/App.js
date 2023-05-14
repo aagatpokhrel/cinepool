1b13363b14375cf73c4ab5ca3afd8225b4673f19
Aagat <aagatpokhrel@gmail.com>
2023-05-14 12:29:24 +0545

    frontend added but not complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect } from 'react';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import './App.css';
 
 function App() {
   const [mediaType, setMediaType] = useState('movie');
