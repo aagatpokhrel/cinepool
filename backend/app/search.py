433ae765ea6ec48dba3323245b2b6ee2b80d4bcf
Aagat <aagatpokhrel@gmail.com>
2023-05-15 16:13:30 +0545

    added backend services

--- backend/app/search.py
+++ backend/app/search.py
@@ -1,2 +1,30 @@
-def search():
-    pass
+from app import collection
+import sentence_transformers
+import numpy as np
+
+def return_embeddings(description):
+    #analyze description
+    model = sentence_transformers.SentenceTransformer('sentence-transformers/bert-base-nli-mean-tokens')
+    embeddings = model.encode(description)
+    arr_list = [float(x) if isinstance(x, np.float32) else x for x in embeddings.tolist()]
+    return arr_list
+
+
+
+def search(data):
+    #search for movies/shows
+    search_query = {}
+    if (data['type']):
+        search_query['type'] = data['type']
+    if (data['genres']):
+        search_query['genres'] = data['genres']
+    if (data['date']):
+        search_query['date'] = data['date']
+    
+    documents = collection.find(search_query)
+
+    if (data['description']):
+        embedding = return_embeddings(data['description'])
+
+    print(documents)
+
